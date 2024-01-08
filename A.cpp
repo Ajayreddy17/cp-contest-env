d525126569746465dc58a62f87ce47a2ce8bd88b
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2024-01-08 20:23:27 +0530

    del

--- A.cpp
+++ A.cpp
@@ -1,8 +1,12 @@
 #include "library/template.hpp"
+#include "library/poly/convolution.hpp"
+
+using mint = modint998;
 
 int main(){
-
-    Int(a, b);
-    print(a - b - b);
+    
+    vector<mint> a = {1, 3, 4, 5};
+    vector<mint> b = {3, 4, 5};
+    print(convolution(a, b));
     return 0;
 }
